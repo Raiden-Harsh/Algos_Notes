60d96dac5a12fcd708ba32e43ce3c06185041d05
harshvardhan.s09 <harshbully@gmail.com>
2022-10-03 20:55:32 +0530

    code added for pattern 5

--- src/patterns/Qfive.java
+++ src/patterns/Qfive.java
@@ -15,9 +15,23 @@ Print this:(n = 5)
 
 public class Qfive {
     public static void main(String[] args) {
+        pattern(5);
 
     }
     static void pattern(int n){
+        for (int i = 0; i < (2*n)-1; i++) {
+            int col;
+            if(i<n){
+               col = i;
+            }else{
+                col =(2*n)-i-2;
+            }
+
+            for (int j = 0; j <col+1 ; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
 
     }
 }
