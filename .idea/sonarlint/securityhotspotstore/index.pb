02dcbbea6e83d1b3b9926103050affb64cf8b0fe
harshvardhan.s09 <harshbully@gmail.com>
2023-05-23 17:06:58 +0530

    singly linked list

--- .idea/sonarlint/securityhotspotstore/index.pb
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -7,3 +7,19 @@ Y
 +src/recursion/permutation/Permutations.java,e\2\e295a0d1bd34c812db146c43c3448bf85b656004
 R
 "src/patterns/Pattern Questions.txt,a\4\a440a4530516c1d70352ab798a84a00c4ff6ccc0
+V
+&src/back_tracking/NKnights_type_2.java,c\d\cd4df1ee77930ab4caa5ae16cc983a134e2134eb
+N
+src/back_tracking/NQueens.java,d\1\d1b19d72399829e49067249ee8bd9ae50ca1f3f0
+S
+#src/back_tracking/back_tracking.txt,4\7\4724c34f7aa294b33a58814e2a3e45146900871e
+O
+src/back_tracking/NKnights.java,f\9\f97f6518af0f4e52668e17a9cd0c16d905c68064
+K
+src/recursion/Recursion.txt,a\a\aa9075dad32fcb79897ae895add39bc85f9f35fe
+K
+src/back_tracking/Maze.java,c\1\c1f97891afab23434d98cb34e53371195c1400f5
+W
+'src/back_tracking/AllDirectionMaze.java,4\e\4e187134e602eb397c46e2bdef70f4e49ddf8e26
+S
+#src/back_tracking/SudokuSolver.java,3\d\3da6d94da9eab5bf7b16a63320d8c9c811182941
---
+++ src/linked_list/main.java
@@ -0,0 +1,42 @@
+package linked_list;
+
+public class main {
+    public static void main(String[] args) {
+        CustomLinkedList linkedList = new CustomLinkedList();
+        linkedList.insertFirst(7);
+        linkedList.insertFirst(6);
+        linkedList.insertFirst(5);
+        linkedList.insertFirst(4);
+        linkedList.insertFirst(3);
+        linkedList.display();
+        System.out.println();
+
+        linkedList.insertLast(12);
+        linkedList.insertLast(18);
+        linkedList.display();
+        linkedList.insert(999,3);
+        System.out.println();
+        linkedList.display();
+        linkedList.deleteFirst();
+        System.out.println();
+        linkedList.display();
+        System.out.println();
+
+
+        // new LinkedList for deletion
+        CustomLinkedList ll = new CustomLinkedList();
+        ll.insertFirst(1);
+        ll.insertFirst(2);
+        ll.insertFirst(3);
+        ll.insertFirst(4);
+        ll.insertFirst(5);
+        ll.insertFirst(99);
+        ll.display();
+        System.out.println(ll.deleteLast());
+        ll.display();
+        System.out.println(ll.delete(2));
+        ll.display();
+        System.out.println(ll.find(5));
+
+    }
+}
